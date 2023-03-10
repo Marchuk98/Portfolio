9ae63d05958ccfb0e6cd517ace40989a23789faf
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-03-10 23:53:16 +0300

    Small edits and additions

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Skills from "./components/skills/Skills";
+import Projects from "./components/projects/Projects";
+import RemoteJob from "./components/remoteJob/RemoteJob";
+import Contacts from "./components/contacts/Contacts";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Header/>
         <Main/>
         <Skills/>
+        <Projects/>
+        <RemoteJob/>
+        <Contacts/>
     </div>
   );
 }
