104d6c950ebe9fe5eb4b3df77e9ab6a9078c8597
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-05-22 23:54:08 +0300

    Working with the Projects component, making changes and adjustments

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import styles from "./Projects.module.css"
-import styleContainer from "../common/Container.module.css"
+import styleContainer from "../common/styles/Container.module.css"
 import ProjectItem from "./projectItem/ProjectItem";
+import Title from "../../features/title/Title";
+import socialProject from '../assets/image/social-network.png';
+import todolistProject from '../assets/image/todolist.jpg';
 
 const Projects = () => {
+
+
     return (
         <div className={styles.projectsBlock}>
             <div className={`${styleContainer.container} ${styles.projectsContainer}`}>
-            <h2 className={styles.title}>My Projects</h2>
+              <Title text={'My project'} description={'Most common methods for designing websites that ' +
+                  'work well on desktop is responsive and adaptive design'}/>
                 <div className={styles.projects}>
-                    <ProjectItem title={"Project title"} description={"Brief description"}/>
-                    <ProjectItem title={"Project title"} description={"Brief description"}/>
-                    <ProjectItem title={"Project title"} description={"Brief description"}/>
-                    <ProjectItem title={"Project title"} description={"Brief description"}/>
+                    <ProjectItem
+                        projectImg={socialProject}
+                        projectLink={'https://vk.com/feed'}
+                        title={"Todolist"}
+                        description={"Here is a todolist developed by me, which will help in planning the workflow."}/>
+                    <ProjectItem
+                        projectImg={todolistProject}
+                        projectLink={'https://todoist.com/ru'}
+                        title={"Social Network"}
+                        description={"A social network is a visual demonstration of the project"}/>
                 </div>
             </div>
         </div>
