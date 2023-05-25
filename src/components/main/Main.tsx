24e4f6a3b13e4f4fa1fe387e16280cdb69c7aff3
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-05-25 23:31:08 +0300

    Stylization and refinement of the main component

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,17 +1,50 @@
-import React from 'react';
-import styles from './Main.module.css'
-import styleContainer from '../common/Container.module.css'
+import React, { useRef, useEffect } from 'react';
+import styles from './Main.module.css';
+import styleContainer from '../common/styles/Container.module.css';
+import Typed from 'typed.js';
+import mySelf from '../assets/image/myPhoto.png'
 
 const Main = () => {
+    const type = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(type.current, {
+            strings: ['Frontend Developer'],
+            typeSpeed: 90,
+            loop: true,
+            backSpeed: 50,
+            startDelay: 800,
+            backDelay: 800
+        });
+        return () => {
+            typed.destroy();
+        };
+    }, []);
+
     return (
-        <div className={styles.mainBlock}>
-            <div className={styleContainer.container}>
-            <div className={styles.text}>
-                <span>Hi There</span>
-                <h1>I am Vladimir Marchuk</h1>
-                <p>Frontend developer</p>
-            </div>
-            <div className={styles.photo}></div>
+        <div id="home" className={styles.mainBlock}>
+            <div className={styles.mainWrapper}>
+                <div className={styleContainer.container}>
+                    <div className={styles.mainWrapperBlock}>
+                        <div className={styles.mainWrapperText}>
+                            <span className={styles.mainGeneralTitle}>Hi There, I am</span>
+                            <h1 className={styles.mainNameTitle}>Vladimir Marchuk</h1>
+                            <div className={styles.mainDynamicPrint}>
+                                <span className={styles.mainPrefixText}>
+                                    A <span ref={type}></span>
+                                </span>
+                            </div>
+                            <p className={styles.mainDescriptionText}>
+                                I'm a frontend developer based in Belarus, and I'm very passionate and dedicated to my
+                                work.
+                            </p>
+                        </div>
+                        <button className={styles.mainButton}>My projects</button>
+                    </div>
+                    <div className={styles.mainPhoto}>
+                        <img className={styles.photo} src={mySelf} alt="Your Photo" />
+                    </div>
+                </div>
             </div>
         </div>
     );
