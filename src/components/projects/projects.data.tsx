266256d00492e1da560fed99eb5446fe0881632e
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-10-12 23:44:52 +0300

    feat: added define project details

---
+++ src/components/projects/projects.data.tsx
@@ -0,0 +1,28 @@
+
+
+export const projects = [
+    {
+        id: 1,
+        title:'Social Web',
+        img: 'cards', /////// редакт
+        linkDeploy: '',
+        linkRepo: '',
+        projectTitle: 'dsfsdf'
+    },
+    {
+        id: 2,
+        title:'Todolist',
+        img: 'cards', /////// редакт
+        linkDeploy: '',
+        linkRepo: '',
+        projectTitle: 'dsfsdf'
+    },
+    {
+        id: 3,
+        title:'Essence-Cards',
+        img: 'cards', /////// редакт
+        linkDeploy: '',
+        linkRepo: '',
+        projectTitle: 'dsfsdf'
+    }
+]
