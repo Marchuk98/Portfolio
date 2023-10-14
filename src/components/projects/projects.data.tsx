e6b5330f3ec33717de00e6702b13d33697c0765a
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-10-14 23:54:57 +0300

    upd: refactor projects component and styles

--- src/components/projects/projects.data.tsx
+++ src/components/projects/projects.data.tsx
@@ -1,28 +1,45 @@
-
+import todolist from '../../components/assets/image/todolist.jpg'
+import socialWeb from '../../components/assets/image/social-network.png'
 
 export const projects = [
     {
         id: 1,
         title:'Social Web',
-        img: 'cards', /////// редакт
+        img: socialWeb, /////// редакт
         linkDeploy: '',
         linkRepo: '',
-        projectTitle: 'dsfsdf'
+        projectTitle: 'Social Web'
     },
     {
         id: 2,
         title:'Todolist',
-        img: 'cards', /////// редакт
+        img: todolist, /////// редакт
         linkDeploy: '',
         linkRepo: '',
-        projectTitle: 'dsfsdf'
+        projectTitle: 'Todolist'
     },
     {
         id: 3,
         title:'Essence-Cards',
-        img: 'cards', /////// редакт
+        img: socialWeb, /////// редакт
         linkDeploy: '',
         linkRepo: '',
-        projectTitle: 'dsfsdf'
+        projectTitle: 'Essence-Cards'
+    },
+    {
+        id: 4,
+        title:'Essence-Cards',
+        img: socialWeb, /////// редакт
+        linkDeploy: '',
+        linkRepo: '',
+        projectTitle: 'Essence-Cards'
+    },
+    {
+        id: 5,
+        title:'Essence-Cards',
+        img: socialWeb, /////// редакт
+        linkDeploy: '',
+        linkRepo: '',
+        projectTitle: 'Essence-Cards'
     }
 ]
