322f4d1296784f944b6905d01477f03515ecc249
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-10-16 23:57:54 +0300

    feat: added controlled-text-field component for react-hook-form integration

---
+++ src/components/controlled-text-field.tsx
@@ -0,0 +1,15 @@
+import {FieldValues, useController, UseControllerProps} from "react-hook-form";
+import {TextField, TextFieldProps} from "./text-field/text-field";
+
+
+type ControlledTextFieldProps<T extends FieldValues> = Omit<UseControllerProps<T>,
+    'reles' | 'defaultValue'> & Omit<TextFieldProps, 'onChang' | 'value'>
+
+
+export const ControlledTextField = <T extends FieldValues>({control, name, ...rest}: ControlledTextFieldProps<T>) => {
+
+    const {field, fieldState: {error}} = useController({name, control})
+
+
+    return <TextField {...field} {...rest} />
+}
