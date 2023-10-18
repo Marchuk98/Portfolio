7234220c4abf3dda8cef76e6c0f48646bc28b2c2
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-10-18 23:55:58 +0300

    feat: added controlled-text-area-field component for react-hook-form integration

---
+++ src/components/controlled-text-area-field.tsx
@@ -0,0 +1,10 @@
+import { FieldValues, useController, UseControllerProps } from 'react-hook-form';
+import { TextAreaField, TextAreaFieldProps } from './text-area-field/text-area-field';
+
+type ControlledTextAreaFieldProps<T extends FieldValues> = Omit<UseControllerProps<T>,
+    'rules' | 'defaultValue'> & Omit<TextAreaFieldProps, 'onChange' | 'value'>;
+
+export const ControlledTextAreaField = <T extends FieldValues>({ control, name, ...rest }: ControlledTextAreaFieldProps<T>) => {
+    const { field, fieldState: { error } } = useController({ name, control });
+    return <TextAreaField {...field} errorMessage={error?.message} {...rest} />;
+};
