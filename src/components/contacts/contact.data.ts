9f413fb346647de51603210309d255e6ccb90da0
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-10-18 23:57:39 +0300

    feat: add contacts data with associated icons

---
+++ src/components/contacts/contact.data.ts
@@ -0,0 +1,9 @@
+import {Address, Email, Phone} from "../assets";
+
+
+export const contacts = [
+    {title: 'Address', infoText: 'Brest, Belarus', IconComponent: Address},
+    {title: 'Phone', infoText: '+375 (29) 826-76-10', IconComponent: Phone},
+    {title: 'Email', infoText: 'vladimir.fmr98@gmail.com', IconComponent: Email},
+    {title: "CV", infoText: 'Vladimir Marchuk', IconComponent: Email}
+]
