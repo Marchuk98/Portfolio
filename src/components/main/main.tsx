410ec0004ca40536b898cba01ae94b177d27afd3
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-10-28 23:59:30 +0300

    upd: refactored Main component and styles for better readability and design.

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef} from 'react';
-import styles from './main.module.scss';
+import s from './main.module.scss';
 import styleContainer from '../common/styles/Container.module.css';
 import Typed from 'typed.js';
 import mySelf from '../assets/image/myPhoto.png'
@@ -24,35 +24,37 @@ export const Main = () => {
     }, []);
 
     return (
-        <div id="home" className={styles.mainBlock}>
-            <div className={styles.mainWrapper}>
-                <div className={styleContainer.container}>
-                    <div className={styles.mainWrapperBlock}>
-                        <div className={styles.mainWrapperText}>
-                            <span className={styles.mainGeneralTitle}>Hi There, I am</span>
-                            <h1 className={styles.mainNameTitle}>Vladimir Marchuk</h1>
-                            <div className={styles.mainDynamicPrint}>
-                                <span className={styles.mainPrefixText}>
+        <section id="home" className={s.mainBlock}>
+            <div className={s.mainWrapper}>
+                <div className={`container ${s.container} `}>
+                    <div className={s.mainWrapperBlock}>
+                        <div className={s.mainWrapperText}>
+                            <span className={s.mainGeneralTitle}>Hi There, I am</span>
+                            <h1 className={s.mainNameTitle}>Vladimir Marchuk</h1>
+                            <div className={s.mainDynamicPrint}>
+                                <span className={s.mainPrefixText}>
                                     A <span ref={type}></span>
                                 </span>
                             </div>
-                            <p className={styles.mainDescriptionText}>
+                            <p className={s.mainDescriptionText}>
                                 I'm a frontend developer based in Belarus, and I'm very passionate and dedicated to my
                                 work.
                             </p>
                         </div>
                     </div>
-                    <div className={styles.mainPhoto}>
-                        <div className={styles.icons}>
-                            <FaReact className={styles.icon} color="#61dafb" />
-                            <SiRedux className={styles.icon} color="#764abc" />
-                            <SiTypescript className={styles.icon} color="#007acc" />
-                            <FaJs className={styles.icon} color="#f7df1e" />
+
+                    <div className={s.mainPhoto}>
+                        <div className={s.icons}>
+                            <FaReact className={s.icon} color="#61dafb" />
+                            <SiRedux className={s.icon} color="#764abc" />
+                            <SiTypescript className={s.icon} color="#007acc" />
+                            <FaJs className={s.icon} color="#f7df1e" />
                         </div>
-                        <img className={styles.photo} src={mySelf} alt="Your Photo" />
+                        <img className={s.photo} src={mySelf} alt="Your Photo" />
                     </div>
+
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
