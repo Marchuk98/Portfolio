c6b061e7035dc6b254e6cbee59d67a7f4346f235
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-11-06 05:43:11 +0300

    upd: reorganized component imports

--- src/components/section/main.tsx
+++ src/components/section/main.tsx
@@ -1,15 +1,14 @@
 import {memo} from "react";
-import {ParticlesContainer} from "../particles/particles-container";
+import {ParticlesContainer} from "../ui/particles/particles-container";
 import {Intro} from "./intro/intro";
-import {Skills} from "../skills/skills";
+import {Skills} from "./skills/skills";
 import {Projects} from "./projects/projects";
 import {RemoteJob} from "./remoteJob/remote-job";
-import {Contacts} from "../contacts/contacts";
+import {Contacts} from "./contacts/contacts";
 
-import s from './main.module.scss'
 export const Main = memo(()=> {
     return (
-        <main className={s.main}>
+        <main>
             <ParticlesContainer />
             <Intro />
             <Skills />
