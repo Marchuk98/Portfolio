1e5bbe570203243b6072de0f22f1dca26464997b
Vladimir Marchuk <Gaba00275@yandex.ru>
2024-01-17 11:40:31 +0300

    upd: updated project images and added links to the deployment and repository.

--- src/components/section/projects/projects.data.tsx
+++ src/components/section/projects/projects.data.tsx
@@ -1,50 +1,33 @@
-import socialWeb from '../../../assets/image/social-network.png'
+import velocityChat from '../../../assets/image/velocity-chat.jpeg'
+import essenseCard from '../../../assets/image/Essense-card.jpg'
 import todolist from '../../../assets/image/todolist.jpg'
 
 export const projects = [
     {
         id: 1,
-        title:'Social Web',
-        img: socialWeb, /////// редакт
-        linkDeploy: '',
-        linkRepo: '',
-        projectTitle: 'Social Web',
+        title:'Essence-Cards',
+        img: essenseCard,
+        linkDeploy: 'https://essence-cards.vercel.app/login',
+        linkRepo: 'https://github.com/Marchuk98/Essence-Cards',
+        projectTitle: 'Essence-Cards',
         projectDescription:'React project with Redux, Axios, TypeScript, React Hook Form'
     },
     {
         id: 2,
-        title:'Todolist',
-        img: todolist, /////// редакт
-        linkDeploy: '',
-        linkRepo: '',
-        projectTitle: 'Essence-Cards',
-        projectDescription:'Description'
+        title:'Task Planner',
+        img: todolist,
+        linkDeploy: 'https://taskcontroller.vercel.app/login',
+        linkRepo: 'https://github.com/Marchuk98/ToDoList',
+        projectTitle: 'Task Planner',
+        projectDescription:'Task Planner is a powerful task management application powered by React, Redux Toolkit, Axios, Material-UI, and React Router.'
     },
     {
         id: 3,
-        title:'Essence-Cards',
-        img: socialWeb, /////// редакт
-        linkDeploy: '',
-        linkRepo: '',
-        projectTitle: 'Todolist',
-        projectDescription:'Description'
-    },
-    {
-        id: 4,
-        title:'Essence-Cards',
-        img: socialWeb, /////// редакт
-        linkDeploy: '',
-        linkRepo: '',
-        projectTitle: 'Name-Project',
-        projectDescription:'Description'
-    },
-    {
-        id: 5,
-        title:'Essence-Cards',
-        img: socialWeb, /////// редакт
-        linkDeploy: '',
-        linkRepo: '',
-        projectTitle: 'Essence-Cards',
+        title:'Velocity Chat',
+        img: velocityChat,
+        linkDeploy: 'https://velocity-chat.vercel.app/',
+        linkRepo: 'https://github.com/Marchuk98/Velocity-Chat',
+        projectTitle: 'Velocity Chat',
         projectDescription:'Description'
     }
 ]
